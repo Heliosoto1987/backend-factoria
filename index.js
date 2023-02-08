c21673aaffc4e75c0634da48f8ed05bd287973db
heliosoto17 <heliosoto17@gmail.com>
2023-02-08 10:40:14 +0100

    pre deploy

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const app = express();
 const { dbConnection } = require("./database/config");
 
 dbConnection();
+
+app.use(cors());
 
 app.use(express.static("public"));
 
